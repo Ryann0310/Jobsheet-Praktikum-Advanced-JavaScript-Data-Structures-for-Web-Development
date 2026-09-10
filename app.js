1e971c76ff78366e9f6367a0a01793440e3230b0
Ryann0310 <ramadhanryan03@students.unnes.ac.id>
2026-09-10 14:51:42 +0700

    bagian pertama

---
+++ app.js
@@ -0,0 +1,29 @@
+//Bagian 1
+// Latihan 1.1
+function calculateDiscountedPrice(price, discountPercent) {
+    return price - (price * discountPercent) / 100;
+}
+
+// Latihan 1.2
+const cart = [
+    { title: "Laptop", price: 1000, discountPercent: 10 },
+    { title: "Mouse", price: 20, discountPercent: 5 },
+    { title: "Keyboard", price: 50, discountPercent: 0 }
+];
+
+function applyDiscounts(cart) {
+    const result = [];
+    for (const item of cart) {
+        const finalPrice = calculateDiscountedPrice(item.price, item.discountPercent);
+        
+        result.push({ 
+            title: item.title, 
+            price: item.price, 
+            discountPercent: item.discountPercent,
+            finalPrice: finalPrice 
+        });
+    }
+    return result;
+}
+
+console.log(applyDiscounts(cart));
